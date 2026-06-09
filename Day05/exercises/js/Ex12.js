a33d43cab64f93fccc646a7a0c200f12a6e446b3
Sharaf-Farhathy <s.farhathy10@gmail.com>
2026-06-09 16:30:27 +0530

    updated

---
+++ Day05/exercises/js/Ex12.js
@@ -0,0 +1,9 @@
+function fc(){
+    cel1 = document.getElementById("c1").value;
+    document.getElementById("result").innerHTML = (cel1 * 1.8) + 32;
+}
+
+function fc(){
+    cel2 = document.getElementById("c2").value;
+    document.getElementById("result").innerHTML = (cel2 * 1.8) + 32;
+}
