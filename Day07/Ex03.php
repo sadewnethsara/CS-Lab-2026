7b588dd35a6b0d8560ae0ef52a4a6bee70ef0a0b
Sharaf-Farhathy <s.farhathy10@gmail.com>
2026-06-23 16:13:04 +0530

    added 4 examples

--- Day07/Ex03.php
+++ Day07/Ex03.php
@@ -1,6 +1,32 @@
 <!DOCTYPE html>
 <html>
 <body>
-    
+    <?php
+		$servername="localhost";
+		$username="root";
+		$password="";
+		$dbname="university";
+		
+		$conn = new mysqli($servername,$username,$password,$dbname);
+		
+		if($conn -> connect_error){
+			die("Connection error" .$conn ->connect_error);
+		}
+		
+		$sql ="Create Table mytable(
+				id INT(7)PRIMARY KEY AUTO_INCREMENT,
+				firstname VARCHAR(30) NOT NULL,
+				lastname VARCHAR(30) NOT NULL,
+				email VARCHAR(50)
+			)";
+			
+		if($conn -> query($sql) == TRUE){
+			echo"Table created Successfully";
+		} else {
+			echo"Error creating table: ".$conn->error;
+		}
+		
+		$conn->close();
+	?>
 </body>
 </html>
--- Day07/Ex04.php
+++ Day07/Ex04.php
@@ -1,6 +1,30 @@
 <!DOCTYPE html>
 <html>
 <body>
-    
+    <?php
+		$servername="localhost";
+		$username="root";
+		$password="";
+		$dbname="university";
+		
+		$conn = new mysqli($servername,$username,$password,$dbname);
+		
+		if($conn -> connect_error){
+			die("Connection error" .$conn ->connect_error);
+		}
+		
+		$sql ="INSERT INTO mytable(firstname,lastname,email) VALUES
+				('Ali','Hashan','ali@example.com'),
+				('Nimal','Perera','nimal@example.com'),
+				('Sara','Fernando','sara@example.com')";
+			
+		if($conn -> query($sql) == TRUE){
+			echo"New record inserted Successfully";
+		} else {
+			echo"Error inserting data: ".$conn->error;
+		}
+		
+		$conn->close();
+	?>
 </body>
 </html>
